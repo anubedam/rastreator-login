30bb8608c912eb77fdb82a440fc16388af082977
Antonio Úbeda Montero <anubedam@gmail.com>
2023-08-01 21:09:08 +0200

    initial call to mail service and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MailService } from './shared/services/mail.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rastreator-login';
+
+  constructor(private mailService: MailService) {}
+
+  ngOnInit(): void {
+    // ONLY FOR DEMO PURPOSE -- Send email test
+    this.mailService.sendEmail().then(result => console.log(result));
+  }
 }
