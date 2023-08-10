696057fc4c4e5715cea66ef306f523b82ee0195e
Antonio Úbeda Montero <anubedam@gmail.com>
2023-08-10 20:09:05 +0200

    Add Ignacio email address and href

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     fromName: 'Antonio Úbeda Montero',
     fromEmail: 'anubedam@gmail.com',
-    hardcodedDestMail: 'anubedam@gmail.com',
+    hardcodedDestMail: 'ignacio.hernandez@rastreator.com',
     mailPublicApiKey: 'fake-public-key',
     mailPrivateApiKey: 'fake-private-key'
 };
