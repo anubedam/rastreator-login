6da452492d61d2fa10bc180018b68ae1d8780919
Antonio Úbeda Montero <anubedam@gmail.com>
2023-08-10 19:30:18 +0200

    add first upload

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
     fromName: 'Antonio Úbeda Montero',
     fromEmail: 'anubedam@gmail.com',
     hardcodedDestMail: 'anubedam@gmail.com',
-    mailPublicApiKey: 'ef2b1c7310a5a4a97f9690236809af21',
-    mailPrivateApiKey: '4cbc440d8d97eccf8da73353e8d8228a'
+    mailPublicApiKey: 'fake-public-key',
+    mailPrivateApiKey: 'fake-private-key'
 };
