469ec83db665780d39c2bb08d06e0d4bd8556c51
Antonio Úbeda Montero <anubedam@gmail.com>
2023-08-10 17:52:02 +0200

    add environments and service

--- src/app/shared/services/mail.service.spec.ts
+++ src/app/shared/services/mail.service.spec.ts
@@ -4,31 +4,45 @@ import { MailService } from './mail.service';
 
 describe('MailService', () => {
   let service: MailService;
+  let callToMailjetSpy: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(MailService);
+    callToMailjetSpy = spyOn(service, 'callToMailjet');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should retrieve http status 200 when email is sent', (done) => {
-    const callToMailjetSpy = spyOn(service, 'callToMailjet').and.returnValue(Promise.resolve(200));
-
-    service.sendEmail().then(resp => {
-      expect(resp).toBe(200);
-      done();
-    });
+  it('email was sent successfully', (done) => {
+    const mailResponse = Promise.resolve({ status: 200 });
+    callToMailjetSpy.and.returnValue(Promise.resolve(mailResponse));
+    const toName = 'pepe';
+    const toEmail = 'fakeMail@gmail.com';
+    const toPhone = '645216037';
+    service.sendEmail(toName, toEmail, toPhone)
+      .then(
+         resp => {
+          expect(callToMailjetSpy).toHaveBeenCalled();
+          done();
+         }
+      );
   });
 
-  it('should retrieve an error when email has any sending error', (done) => {
-    const callToMailjetSpy = spyOn(service, 'callToMailjet').and.returnValue(Promise.reject('there was an error'));
-
-    service.sendEmail().catch(err => {
-      expect(err).toBe('there was an error');
-      done();
-    });
+  it('email not was sent successfully (there was an error)', (done) => {
+    const mailResponse = Promise.resolve({ status: 500, message: 'Server error' });
+    callToMailjetSpy.and.returnValue(Promise.reject(mailResponse));
+    const toName = 'pepe';
+    const toEmail = 'fakeMail@gmail.com';
+    const toPhone = '645216037'; 
+    service.sendEmail(toName, toEmail, toPhone)
+      .catch(
+         err => {
+          expect(callToMailjetSpy).toHaveBeenCalled();
+          done();
+         }
+      );
   });
 });
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    fromName: 'Antonio Úbeda Montero',
+    fromEmail: 'anubedam@gmail.com',
+    mailPublicApiKey: 'ef2b1c7310a5a4a97f9690236809af21',
+    mailPrivateApiKey: '4cbc440d8d97eccf8da73353e8d8228a'
+};
