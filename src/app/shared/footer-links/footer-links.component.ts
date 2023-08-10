624a0959744946da5518c9d73cfa53e0596a59c8
Antonio Úbeda Montero <anubedam@gmail.com>
2023-08-10 18:44:27 +0200

    add rastreator real footer links

--- src/app/shared/footer-links/footer-links.component.ts
+++ src/app/shared/footer-links/footer-links.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FooterLegalOptions } from '../interfaces/footer-legal-options.interface';
 
 @Component({
   selector: 'app-footer-links',
@@ -6,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-links.component.scss']
 })
 export class FooterLinksComponent implements OnInit{
-  legalOptions: string[] = [];
+  legalOptions: FooterLegalOptions[] = [];
   adviceText: string = '';
 
   ngOnInit(): void {
     this.legalOptions = [
-      'Aviso Legal',
-      'Política de Cookies',
-      'Política de Privacidad'
+      { text: 'Aviso Legal', url: 'https://www.rastreator.com/aviso-legal.aspx' },
+      { text: 'Política de Cookies', url: 'https://www.rastreator.com/politica-de-cookies.aspx' },
+      { text: 'Política de Privacidad', url: 'https://www.rastreator.com/politica-de-privacidad.aspx' }
     ];
     this.adviceText = 'Recuerda revisar la información básica de privacidad'
   }
